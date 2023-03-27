fbeeb49eef4c2f4df53e2ec12301d0d72dcc8f51
Romain Touchet <romain.touchet@gmail.com>
2023-03-27 15:14:06 +0200

    viré quelques lignes vides

--- index.php
+++ index.php
@@ -2,17 +2,13 @@
 $title="Accueil";
 require_once __DIR__. '/classes/ConnexionMessages.php';
 session_start();
-if (!isset($_SESSION['isConnected']))
-{
+if (!isset($_SESSION['isConnected'])){
     $_SESSION['isConnected']=false;
 }
 $_SESSION['fromWhichPage']='index.php';
 require_once __DIR__ . '/layout/header.php';
 ?>
 
-
-    
-    
 <div class="background container-fluid d-flex flex-column justify-content-center">
     <?php if (array_key_exists('msg', $_GET)) { ?>
         <div class="alert alert-warning my-1 mx-auto" role="alert">
@@ -24,7 +20,6 @@ require_once __DIR__ . '/layout/header.php';
         <p class="mx-auto">Repassez plus tard !</p>
     </div>
 </div>
-
 
 <?php
 var_dump($_SESSION);
