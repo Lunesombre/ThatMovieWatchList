77ef43cc51d0e17bca9b6ba6fc5e3385d0764ddc
Romain Touchet <romain.touchet@gmail.com>
2023-04-04 01:56:49 +0200

    updated database and added different artists.php displays according to artists genders

--- artist.php
+++ artist.php
@@ -39,33 +39,40 @@ if (isset($actor_id)) {
         </div>
         <div class="m-5 align-items-between">
             <h2 class="text-end">
-                <?php echo $actor['actor_firstname'] . ' ' . $actor['actor_name'] ?> est un/e acteur/trice.
+                <?php echo $actor['actor_firstname'] . ' ' . $actor['actor_name']; ?> est
+                <?php if ($actor['actor_gender'] === 1) { ?>
+                    une actrice.
+            </h2>
+            <h3>Elle a notamment joué dans :</h3>
+            <?php } else { ?>
+                un acteur.
             </h2>
             <h3>Il a notamment joué dans :</h3>
-            <p>
-                <?php $query = "SELECT * FROM l_actor_movie NATURAL JOIN movie WHERE actor_id=$actor_id";
-                $stmt = $pdo->query($query);
-                $thisActorMovies = $stmt->fetchAll(); ?>
-            <div class="d-flex flex-row flex-wrap">
-                <?php foreach ($thisActorMovies as $result) {     ?>
-                    <a href="movie.php?movie_id=<?php echo $result['movie_id'] ?>" class="searchResults text-decoration-none d-flex align-items-around p-3 m-2 rounded-3">
-                        <div class="d-flex flex-column justify-content-between">
-                            <h3><?php echo $result['movie_name'] ?></h3>
-                            <div class="minipic rounded">
-                                <img src="<?php echo $result['movie_poster'] ?>" class="img-fluid rounded" alt="poster du film <?php echo $result['movie_name'] ?>">
-                            </div>
+            <?php } ?>
+        <p>
+            <?php $query = "SELECT * FROM l_actor_movie NATURAL JOIN movie WHERE actor_id=$actor_id";
+            $stmt = $pdo->query($query);
+            $thisActorMovies = $stmt->fetchAll(); ?>
+        <div class="d-flex flex-row flex-wrap">
+            <?php foreach ($thisActorMovies as $result) {     ?>
+                <a href="movie.php?movie_id=<?php echo $result['movie_id'] ?>" class="searchResults text-decoration-none d-flex align-items-around p-3 m-2 rounded-3">
+                    <div class="d-flex flex-column justify-content-between">
+                        <h3><?php echo $result['movie_name'] ?></h3>
+                        <div class="minipic rounded">
+                            <img src="<?php echo $result['movie_poster'] ?>" class="img-fluid rounded" alt="poster du film <?php echo $result['movie_name'] ?>">
                         </div>
-                    </a>
-                <?php } ?>
-            </div>
-            </p>
-            <div class="d-flex justify-content-end">
-                <?php if ($backToThatPage !== 'dashboard.php') { ?>
-                    <a href='searchResults.php' class="align-self-end btn btn-outline-dark btn-sm">Retour à la page de recherche</a>
-                    <?php } else { ?>
-                        <a href="dashboard.php" class="align-self-end btn btn-outline-dark btn-sm">Retour à mon profil</a>
-                <?php } ?>
-            </div>
+                    </div>
+                </a>
+            <?php } ?>
+        </div>
+        </p>
+        <div class="d-flex justify-content-end">
+            <?php if ($backToThatPage !== 'dashboard.php') { ?>
+                <a href='searchResults.php' class="align-self-end btn btn-outline-dark btn-sm">Retour à la page de recherche</a>
+            <?php } else { ?>
+                <a href="dashboard.php" class="align-self-end btn btn-outline-dark btn-sm">Retour à mon profil</a>
+            <?php } ?>
+        </div>
         </div>
     </div>
 <?php
@@ -91,9 +98,16 @@ if (isset($actor_id)) {
         </div>
         <div class="m-5 align-items-between">
             <h2 class="text-end">
-                <?php echo $director['director_firstname'] . ' ' . $director['director_name'] ?> est un/e réalisateur/trice.
+                <?php echo $director['director_firstname'] . ' ' . $director['director_name'] ?>  est
+                <?php if ($director['director_gender'] === 1) { ?>
+                    une réalisatrice.
+            </h2>
+            <h3>Elle a notamment réalisé :</h3>
+            <?php } else { ?>
+                un réalisateur.
             </h2>
             <h3>Il a notamment réalisé :</h3>
+            <?php } ?>
             <p>
                 <?php $query = "SELECT * FROM l_director_movie NATURAL JOIN movie WHERE director_id=$director_id";
                 $stmt = $pdo->query($query);
@@ -112,10 +126,10 @@ if (isset($actor_id)) {
             </div>
             </p>
             <div class="d-flex justify-content-end">
-            <?php if ($backToThatPage !== 'dashboard.php') { ?>
+                <?php if ($backToThatPage !== 'dashboard.php') { ?>
                     <a href='searchResults.php' class="align-self-end btn btn-outline-dark btn-sm">Retour à la page de recherche</a>
-                    <?php } else { ?>
-                        <a href="dashboard.php" class="align-self-end btn btn-outline-dark btn-sm">Retour à mon profil</a>
+                <?php } else { ?>
+                    <a href="dashboard.php" class="align-self-end btn btn-outline-dark btn-sm">Retour à mon profil</a>
                 <?php } ?>
             </div>
         </div>
